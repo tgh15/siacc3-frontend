fb13e274bb48b2fa3744fa457ddc5f745b8c0f06
amate.e <amatee34@gmail.com>
2022-11-05 12:44:27 +0700

    update aktifitas pengguna

--- src/services/pages/configuration/user-activity/Get.js
+++ src/services/pages/configuration/user-activity/Get.js
@@ -1,7 +1,8 @@
 import FetchServices from "../../../core/Axios";
 
-const Get = ({params,onSuccess,onFail}) => {
+const Get = ({params, onSuccess, onFail}) => {
     const service = new FetchServices();
+
     service.get("api-gw/activities", params).then(response => {
         onSuccess(response.data)
     }).catch(err => {
