8bf0ad5ba169a821d43de7ba0354b802c983b1ca
Fikri Abdillah <fikriabdillah95@gmail.com>
2022-12-19 13:55:48 +0800

    update email validation for workunit

--- src/views/configuration/work-unit-list/validation.js
+++ src/views/configuration/work-unit-list/validation.js
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 export const validation = yup.object().shape({
     code                : yup.string().max(10, 'Jumlah maksimal karakter adalah 10!').required("Kolom ID belum terisi"),
     name                : yup.string().required("Kolom nama belum terisi"),
-    email               : yup.string().email().required("Kolom email belum terisi"),
+    // email               : yup.string().email().required("Kolom email belum terisi"),
     address             : yup.string().required("Kolom alamat belum terisi"),
     latitude            : yup.string().required("Kolom garis lintang belum terisi"),
     longitude           : yup.string().required("Kolom garis bujur belum terisi"),
